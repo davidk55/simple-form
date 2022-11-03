1cb433d311d20f1cc53fd23691af1387839a64b4
David <david.programming@protonmail.com>
2022-11-03 16:28:26 +0100

    feat: add Attribution component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
+import Attribution from './Attribution';
 import Form from './Form';
 
 function App() {
   return (
     <div className='flex h-screen flex-col items-center justify-between'>
       <Form />
+      <Attribution />
+    </div>
   );
 }
 
---
+++ src/components/Attribution.jsx
@@ -0,0 +1,14 @@
+const Attribution = () => {
+  return (
+    <footer className='mb-6 rounded-xl bg-gray-800 bg-opacity-75 py-2  px-8 text-white'>
+      <p className='text-sm tracking-wide'>
+        Made by{' '}
+        <a className='text-cyan-400' href='https://github.com/davidk55'>
+          DavidK55
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
+export default Attribution;
