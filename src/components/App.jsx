bfc6df1a6680388f2b088847b05772d93713ead2
David <david.programming@protonmail.com>
2022-11-03 15:25:01 +0100

    feat: generate remaining inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,49 @@ function App() {
     confirmPassword: '',
   });
 
+  const inputs = [
+    {
+      id: 1,
+      name: 'firstName',
+      type: 'text',
+      placeholder: 'First Name',
+      label: 'First Name',
+      required: true,
+    },
+    {
+      id: 2,
+      name: 'lastName',
+      type: 'text',
+      placeholder: 'Last Name',
+      label: 'Last Name',
+      required: true,
+    },
+    {
+      id: 3,
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email',
+      label: 'Email',
+      required: true,
+    },
+    {
+      id: 4,
+      name: 'password',
+      type: 'password',
+      placeholder: 'Password',
+      label: 'Password',
+      required: true,
+    },
+    {
+      id: 5,
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Confirm Password',
+      label: 'Confirm Password',
+      required: true,
+    },
+  ];
+
   function handleSubmit(event) {
     event.preventDefault();
     // submit to API
@@ -25,6 +68,17 @@ function App() {
     });
   }
 
+  const remainingElements = inputs
+    .slice(2)
+    .map((input) => (
+      <FormInput
+        key={input.id}
+        {...input}
+        handleChange={handleChange}
+        value={formData[input.name]}
+      />
+    ));
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -35,12 +89,19 @@ function App() {
       </h1>
       <div className='flex flex-col gap-6 md:flex-row'>
         <FormInput
+          key={inputs[0].id}
+          {...inputs[0]}
           handleChange={handleChange}
+          value={formData[inputs[0].name]}
         />
         <FormInput
+          key={inputs[1].id}
+          {...inputs[1]}
           handleChange={handleChange}
+          value={formData[inputs[1].name]}
         />
       </div>
+      {remainingElements}
       <button
         className='mt-2 mb-1 rounded bg-blue-900 py-2 font-bold text-white'
         type='submit'
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+
 function FormInput(props) {
+  const [focused, setFocused] = useState(false);
+
+  function handleFocus() {
+    setFocused(true);
+  }
+
   return (
     <div className='flex flex-col justify-center w-full'>
+      <label className='block mb-1'>{props.placeholder}</label>
+      <input
+        placeholder={props.placeholder}
+        className='border rounded w-full py-1 px-2 bg-my-white peer aria-checked:invalid:border-red-500'
+        value={props.value}
         onChange={props.handleChange}
         name={props.name}
         type={props.type}
+        required={props.required}
+        pattern={props.pattern}
+        onBlur={handleFocus}
+        aria-checked={focused.toString()}
+        onFocus={() => props.name == 'confirmPassword' && setFocused(true)}
+      />
     </div>
   );
 }
